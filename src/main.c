a24a6852c28f7898cd359f7371517ccc74c7dbcb
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2025-07-01 09:42:28 +0200

    update mask for cross compa

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: maximemartin <maximemartin@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 15:15:08 by maximemarti       #+#    #+#             */
-/*   Updated: 2025/06/30 09:12:47 by maximemarti      ###   ########.fr       */
+/*   Updated: 2025/07/01 09:41:58 by maximemarti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ void	game_setup(int ac, char **av, t_game *game)
 void	game_loop(t_game *game)
 {
 	mlx_hook(game->win, 17, 0, close_window, game);
-	mlx_hook(game->win, 2, 0, key_press, game);
-	mlx_hook(game->win, 3, 0, key_release, game);
+	mlx_hook(game->win, 2, 1L << 0, key_press, game);
+	mlx_hook(game->win, 3, 1L << 1, key_release, game);
 	mlx_hook(game->win, 6, 0, mouse_motion, game);
 	mlx_loop_hook(game->mlx, render_loop, game);
 	mlx_loop(game->mlx);
